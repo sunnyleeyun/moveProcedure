998fcb868df76df1514f6eeb5d245d66b29348a5
sunnyleeyun <sunnylee945@gmail.com>
2016-11-09 23:54:31 +0800

    Initial Commit

---
+++ moveProcedure/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  moveProcedure
+//
+//  Created by Sunny on 2016/11/9.
+//  Copyright © 2016年 Sunny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
